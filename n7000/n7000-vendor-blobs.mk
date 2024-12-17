ff292f7481c6e4c9bb101caa855e871871603c73
zhafknight <96339831+zhafknight@users.noreply.github.com>
2024-12-18 00:22:03 +0800

    Update n7000-vendor-blobs.mk

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -28,6 +28,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/bcmdhd_sta.bin:system/vendor/etc/wifi/bcmdhd_sta.bin \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_mfg.txt:system/vendor/etc/wifi/nvram_mfg.txt \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_mfg.txt_murata:system/vendor/etc/wifi/nvram_mfg.txt_murata \
+    $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_mfg.txt_semcove:system/vendor/etc/wifi/nvram_mfg.txt_semcove \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_net.txt:system/vendor/etc/wifi/nvram_net.txt \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_net.txt_murata:system/vendor/etc/wifi/nvram_net.txt_murata \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/nvram_net.txt_semcove:system/vendor/etc/wifi/nvram_net.txt_semcove
